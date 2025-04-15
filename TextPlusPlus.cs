a0286d80a937dbc8d4ef8e065cd09b144ec58bb9
King Bingus <bendawsdev@gmail.com>
2025-04-15 15:09:56 -0500

    math thing

--- TextPlusPlus.cs
+++ TextPlusPlus.cs
@@ -91,4 +91,75 @@ namespace TextPlusPlus
             return literalContents;
         }
     }
+
+    // handler for math functions on TXT++
+    internal class Math
+    {
+        internal static bool IsMathOperation(char c)
+        {
+            string MathCharacters = "+-*/^";
+
+            foreach (char mc in MathCharacters) {
+                if (c == mc) return true;
+            }
+
+            return false;
+        }
+
+        internal static float ProcessMath(string query2)
+        {
+            string query = query2.Replace(' ', '');
+            string n1;
+            string n2;
+            bool _1 = false;
+            string operation;
+
+            for (int strIdx = 0; strIdx < query.Length; strIdx++)
+            {
+                if (!IsMathOperation(query[strIdx]))
+                {
+                    if (_1)
+                        n2 += query[strIdx];
+                    else
+                        n1 += query[strIdx];
+                } else
+                {
+                    switch (query[strIdx])
+                    {
+                        case "+":
+                            operation = "add";
+                            break;
+                        case "-":
+                            operation = "sub";
+                            break;
+                        case "*":
+                            operation = "mul";
+                            break;
+                        case "/":
+                            operation = "div";
+                            break;
+                        case "^":
+                            operation = "pwr";
+                            break;
+                    }
+                }
+            }
+
+            switch (operation)
+            {
+                case "add":
+                    return float.TryParse(n1) + float.TryParse(n2);
+                case "sub":
+                    return float.TryParse(n1) - float.TryParse(n2);
+                case "mul":
+                    return float.TryParse(n1) * float.TryParse(n2);
+                case "div":
+                    return float.TryParse(n1) / float.TryParse(n2);
+                case "pwr":
+                    return float.TryParse(n1)^float.TryParse(n2);
+            }
+
+            return "";
+        }
+    }
 }
